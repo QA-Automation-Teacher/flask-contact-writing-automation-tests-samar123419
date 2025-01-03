2bf7b2237ad0ccbd07e1146bb73da9a3b659398a
Samar Hdeeb <samar.abu.hdeeb@gmail.com>
2025-01-03 11:14:49 +0200

    integration tests

--- tests/integration/test_api_methods.py
+++ tests/integration/test_api_methods.py
@@ -12,7 +12,7 @@ def teardown_module(module):
 
 
 def test_get_contacts(client):
-    response = client.get(BASE_URL + "/contacts")
+    response = client.get(BASE_URL + "/api/contacts")
     assert response.status_code == 200
     assert isinstance(response.get_json(), list)
 
@@ -32,8 +32,8 @@ def test_response_time_delete(client):
 
     assert response_time < 300
 
-def test_name_in_users(client):
-    response = client.get( BASE_URL + "/contacts")
+def test_name_in_contacts(client):
+    response = client.get( BASE_URL + "/api/contacts")
     # print(response.text)
     data = response.get_json()
     num = len(data)
@@ -41,8 +41,8 @@ def test_name_in_users(client):
     for i in range(num):
         assert "name" in data[i].keys()
 
-def test_email_in_users(client):
-    response = client.get( BASE_URL + "/contacts")
+def test_email_in_contacts(client):
+    response = client.get( BASE_URL + "/api/contacts")
     # print(response.text)
     data = response.get_json()
     num = len(data)
@@ -50,8 +50,8 @@ def test_email_in_users(client):
     for i in range(num):
         assert "email" in data[i].keys()
 
-def test_surname_in_users(client):
-    response = client.get( BASE_URL + "/contacts")
+def test_surname_in_contacts(client):
+    response = client.get( BASE_URL + "/api/contacts")
     # print(response.text)
     data = response.get_json()
     num = len(data)
@@ -59,8 +59,8 @@ def test_surname_in_users(client):
     for i in range(num):
         assert "surname" in data[i].keys()
 
-def test_phone_in_users(client):
-    response = client.get( BASE_URL + "/contacts")
+def test_phone_in_contacts(client):
+    response = client.get( BASE_URL + "/api/contacts")
     # print(response.text)
     data = response.get_json()
     num = len(data)
@@ -68,7 +68,18 @@ def test_phone_in_users(client):
     for i in range(num):
         assert "phone" in data[i].keys()
 
+# def test_check_amount_of_posts(client):
+#     res1 = client.get(BASE_URL + "/api/contacts")
 
+#     before_delete_post = res1.get_json()
+#     print(len(before_delete_post))
+#     response = client.delete(BASE_URL+ "/api/contacts/delete/28")
+
+#     res2 = client.get(BASE_URL + "/api/contacts")
+#     data_after =res2.get_json() 
+#     print(len(data_after))
+    
+#     assert len(data_after) < len(before_delete_post)
 
 
 
